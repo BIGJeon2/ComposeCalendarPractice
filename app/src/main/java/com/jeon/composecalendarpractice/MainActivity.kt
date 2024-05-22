c6fbede0a3619c425ebe171f1d233b8bc05e4335
BIGJeon2 <robert1157311@gmail.com>
2024-05-22 15:54:04 +0900

    Create Header

--- app/src/main/java/com/jeon/composecalendarpractice/MainActivity.kt
+++ app/src/main/java/com/jeon/composecalendarpractice/MainActivity.kt
@@ -4,14 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.jeon.composecalendarpractice.ui.theme.ComposeCalendarPracticeTheme
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +36,125 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeCalendarPracticeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                CalenderPractice()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+private fun CalenderPractice(){
+    val calenderInstance = Calendar.getInstance()
+    val time = remember {
+        mutableStateOf(calenderInstance)
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CalenderHeader(time)
+        CalendarHeaderBtn(time)
+        CalendarDayName(time)
+        CalendarBody(time)
+    }
+
+}
+
+@Composable
+private fun CalenderHeader(date: MutableState<Calendar>){
+    // xxxx년 xx월
+    val resultTime = SimpleDateFormat("yyyy년 MM월", Locale.KOREA).format(date.value.time)
+
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = resultTime,
+        fontSize = 30.sp
     )
+
+}
+
+@Composable
+private fun CalendarHeaderBtn(date: MutableState<Calendar>){
+    // xxxx년 xx월
+    val resultTime = SimpleDateFormat("yyyy년 MM월", Locale.KOREA).format(date.value.time)
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 30.dp, bottom = 30.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Button(
+            onClick = {
+                val newDate = Calendar.getInstance()
+                newDate.time = date.value.time
+                newDate.add(Calendar.MONTH, -1)
+                date.value = newDate
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Black
+            )
+        ) {
+            Text(
+                text = "<",
+                color = Color.Green
+            )
+        }
+
+        Button(
+            onClick = {
+                val newDate = Calendar.getInstance()
+                newDate.time = date.value.time
+                newDate.add(Calendar.MONTH, +1)
+                date.value = newDate
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Black
+            )
+        ) {
+            Text(
+                text = ">",
+                color = Color.Green
+            )
+        }
+    }
+}
+
+@Composable
+private fun CalendarDayName(date: MutableState<Calendar>){
+    val nameList = listOf("일","월","화","수","목","금","토")
+    Row(
+        modifier = Modifier
+            .padding(10.dp),
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
+        nameList.forEach {
+            Box(
+                modifier = Modifier
+                    .weight(1f),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = it,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.ExtraBold
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun CalendarBody(date: MutableState<Calendar>){
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeCalendarPracticeTheme {
-        Greeting("Android")
+        CalenderPractice()
     }
 }
